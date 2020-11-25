178efbba740e519c92f1f941cf1d71db695f3559
eugenmorok <64523467+eugenmorok@users.noreply.github.com>
2020-11-25 20:57:25 +0700

    25-11-2020-1

---
+++ src/AlignedOutputArrayInInt.java
@@ -0,0 +1,39 @@
+public class AlignedOutputArrayInInt {
+
+    public static void outputArrayInInteger(int numberOfElementsInLine, String nameOfTheOutputArray) {
+
+        int[] sArray = MyEasyGeneratorOfPosForCourse1.getMineGenArray(10);
+
+        System.out.println(nameOfTheOutputArray);
+
+        int rowLength = 12;
+        String oneCell = "";
+
+        for (int i = 1; i < sArray.length + 1; i++) {
+
+            oneCell = "[" + (i - 1) + "] = " + sArray[i-1];
+
+            System.out.print(oneCell);
+
+            for (int k = 0; k < rowLength - oneCell.length(); k++) {
+
+                System.out.print(" ");
+
+            }
+
+            System.out.print(" ");
+
+            if (i % numberOfElementsInLine == 0) System.out.println("\n");
+
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        outputArrayInInteger(3, "The array of random values:\n");
+
+    }
+
+}
---
+++ src/OutputOfTheArrayInIntegerWithIndexes.java
@@ -0,0 +1,25 @@
+public class OutputOfTheArrayInIntegerWithIndexes {
+
+    public static void outputArrayInInteger(int numberOfElementsInLine, String nameOfTheOutputArray) {
+
+        int[] sArray = MyEasyGeneratorOfPosForCourse1.getMineGenArray(10);
+
+        System.out.println(nameOfTheOutputArray);
+
+        for (int i = 1; i < sArray.length + 1; i++) {
+
+            System.out.printf("[%d] = %d      ", i - 1, sArray[i - 1]);
+            if (i % numberOfElementsInLine == 0) System.out.println("\n");
+
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        outputArrayInInteger(3, "The array of random values:\n");
+
+    }
+
+}
